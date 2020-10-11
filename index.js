e77065737b4b13ad6abbfc7bc0d36326063dcba4
Feroj <khapangiferoj@gmail.com>
2020-10-10 21:05:55 -0600

    added container, questios and asnwer buttons

---
+++ index.js
@@ -0,0 +1,38 @@
+let start = document.getElementById("start");
+let quiz = document.getElementById("quiz");
+let start = document.getElementById("question");
+let start = document.getElementById("counter");
+let start = document.getElementById("timeGauge");
+
+let choiceA = document.getElementById("A");
+let choiceB = document.getElementById("B");
+let choiceC = document.getElementById("C");
+
+let scoreContainer = document.getElementById("scoreContainer");
+
+var questions = [{
+        question: "What does HTML stand for?",
+        choiceA = "Hype The Matt Lad",
+        choiceB = "Hyper Text Markup Language",
+        choiceC = "Hippo Tik Mark Language",
+        correct = "A"
+
+    },
+
+    {
+        question: "What does HTML stand for?",
+        choiceA = "Hype The Matt Lad",
+        choiceB = "Hyper Text Markup Language",
+        choiceC = "Hippo Tik Mark Language",
+        correct = "A"
+    },
+
+    {
+        question: "What does HTML stand for?",
+        choiceA = "Hype The Matt Lad",
+        choiceB = "Hyper Text Markup Language",
+        choiceC = "Hippo Tik Mark Language",
+        correct = "A"
+    }
+
+]
